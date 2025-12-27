6f70599a43826fa1f3e8332ffade539364572b8a
cremope <novoa.lopez@gmail.com>
2025-12-27 18:37:38 -0300

    Ajusta aquivos de page e steps de login

--- src/main/java/page/LoginPage.java
+++ src/main/java/page/LoginPage.java
@@ -1,40 +1,49 @@
 package page;
 
 import com.microsoft.playwright.Page;
+import config.ConfigLoader;
+import io.qameta.allure.Allure;
+import utils.ScreenshotUtil;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginPage {
     private final Page page;
-    private final String inputUsuario = "#usuario";
-    private final String inputSenha = "#senha";
-    private final String btnEntrar = "#btn-entrar";
-    private final String lblMensagem = "#mensagem";
+
+    private static final String USER = "#user-name";
+    private static final String PASS = "#password";
+    private static final String BTN_LOGIN = "#login-button";
+    private static final String INVENTORY_LIST = ".inventory_list";
 
     public LoginPage(Page page) {
         this.page = page;
     }
 
-    public void abrirSite(String url) {
-        this.page.navigate(url);
+    public void abrirSite() {
+        Allure.step("Navegando para o site SauceDemo");
+        String url = ConfigLoader.getUrlSauceDemo();
+        page.navigate(url);
+        ScreenshotUtil.capturarEAnexar(page, "Evidencia");
     }
 
-    public void preencherUsuario(String usuario) {
-
-        page.fill(inputUsuario, usuario);
+    public void logar(String usuario, String senha) {
+        Allure.step("Realizando login com o usuário: " + usuario);
+        page.fill(USER, usuario != null ? usuario : "");
+        page.fill(PASS, senha != null ? senha : "");
+        ScreenshotUtil.capturarEAnexar(page, "Evidencia");
+        page.click(BTN_LOGIN);
     }
 
-    public void preencherSenha(String senha) {
-        page.fill(inputSenha, senha);
+    public void validarLoginSucesso() {
+        Allure.step("Validando se a vitrine de produtos está visível");
+        assertTrue(page.isVisible(INVENTORY_LIST), "Erro: Navegação pós-login falhou!");
+        ScreenshotUtil.capturarEAnexar(page, "Evidencia");
     }
 
-    public void clicarEntrar() {
-        page.click(btnEntrar);
-    }
-
-    public void ValidaMensagem(String mensagemEsperada) {
-        page.waitForSelector("#mensagem");
-        String mensagemTela = page.textContent(lblMensagem).trim();
-        if (!mensagemTela.equals(mensagemEsperada)) {
-            throw new AssertionError("Mensagem esperada: '" + mensagemEsperada + "' Mensagem encontrada: '" + mensagemTela + "'");
-        }
+    public void validarMensagemErro(String msg) {
+        Allure.step("Validando se a mensagem de erro contém: " + msg);
+        String erro = page.textContent("[data-test='error']");
+        assertTrue(erro.contains(msg), "Mensagem de erro não encontrada!");
+        ScreenshotUtil.capturarEAnexar(page, "Evidencia");
     }
 }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,55 +1,33 @@
 package steps;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.Então;
-import io.cucumber.java.pt.Quando;
+import io.cucumber.java.pt.*;
 import page.LoginPage;
-import utils.ScreenshotUtil;
 
 public class LoginSteps {
-    private Playwright playwright;
-    private Browser browser;
-    private Page page;
-    LoginPage loginPage;
+    private LoginPage loginPage;
 
-    @Before
-    public void setup() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(true)
-                .setSlowMo(400)
-        );
-        page = browser.newPage();
-        loginPage = new LoginPage(page);
+    @Before(order = 1)
+    public void initPages() {
+        this.loginPage = new LoginPage(Hooks.getPage());
     }
 
-    @After
-    public void teardown() {
-        ScreenshotUtil.capturarEAnexar(page, "Evidencia");
-        browser.close();
-        playwright.close();
+    @Dado("que estou na página de login do SauceDemo")
+    public void navegar() {
+        loginPage.abrirSite();
     }
 
-    @Dado("que informo o usuario {string} e a senha {string}")
-    public void que_informo_o_usuario_e_a_senha(String usuario, String senha) {
-        loginPage.abrirSite("https://cremope.github.io/RCremonez/");
-        loginPage.preencherUsuario(usuario);
-        loginPage.preencherSenha(senha);
+    @Quando("tento logar com o usuário {string} e senha {string}")
+    public void logar(String usuario, String senha) {
+        loginPage.logar(usuario, senha);
     }
 
-    @Quando("clico no botão entrar")
-    public void clico_no_botao_entrar() {
-        loginPage.clicarEntrar();
-    }
-
-    @Então("sistema apresentará uma mensagem de {string}")
-    public void sistema_apresentara_uma_mensagem(String mensagemEsperada) {
-        loginPage.ValidaMensagem(mensagemEsperada);
+    @Então("devo validar o resultado esperado {string}")
+    public void validarResultado(String resultado) {
+        if (resultado.equals("LOGIN_SUCESSO")) {
+            loginPage.validarLoginSucesso();
+        } else {
+            loginPage.validarMensagemErro(resultado);
+        }
     }
 }
